07f25ac9eed154b8002b24ef100fe96272e2100a
annejunkes <anneburkhardtjunkes@gmail.com>
2025-03-31 08:18:02 -0300

    metodos ciclo de vida

--- pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ifsc.contaclick;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("ciclo de vida", "método onCreate");
         setContentView(R.layout.activity_main);
 
         edAltura =  findViewById(R.id.edAltura);
@@ -31,14 +35,45 @@ public class MainActivity extends AppCompatActivity {
 
             Double peso = Double.parseDouble(edPeso.getText().toString());
             Double altura = Double.parseDouble(edAltura.getText().toString());
-
             Double imc = peso/(altura*altura);
 
-            String imcTxt = imc.toString();
-            tvIMC.setText(imcTxt);
+            DecimalFormat decimalFormat = new DecimalFormat( "##.##");
+            tvIMC.setText(decimalFormat.format(imc));
 
         });
 
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclo de vida", "método onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclo de vida", "método onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ciclo de vida", "método onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo de vida", "método onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ciclo de vida", "método oStop");
+    }
+
+
 }
