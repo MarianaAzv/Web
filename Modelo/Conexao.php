e6c5601c2a118c603939830e6d8c29d5a585b360
mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-06-18 12:17:57 -0300

    Aula do dia 18-06 parte 2

---
+++ Modelo/Conexao.php
@@ -0,0 +1,16 @@
+<?php
+ class Conexao{
+    public static function getConexao(){
+
+       // mysqli__Connect("localhost","usuario","senha","bd");
+
+        //Class PDO conection 
+       //new PDO('mysql:host=localhost;dbname=test',$user,$pass);
+      //pode passar outras constates/parametros(SITE da documentação: php.net)
+
+        return new PDO('mysql:host=localhost;dbname=crud',"root","admin", [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTIONs]) ;
+    }
+ }
+
+
+?>
