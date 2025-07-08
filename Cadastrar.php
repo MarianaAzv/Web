2094ca8aaf5b9602594b3c9286753554c62bce25
Mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-07-08 18:55:45 -0300

    Lista Js

---
+++ Cadastrar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action = "rota.php?rota=Inserir" method="post">
+
+<label for="nome">Nome</label>
+<input type= "nome" id="nome" name="nome">
+
+
+<label for="senha">senha</label>
+<input type= "senha" id="senha" name="senha">
+
+<label for="email">email</label>
+<input type="email" id="email" name="email">
+
+<input type="submit" value="Cadastrar">
+   </form> 
+</body>
+</html>
